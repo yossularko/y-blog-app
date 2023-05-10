acd87dcb4af430ab956ce711e949e927e373182a
radenlarko <radenlarko@gmail.com>
2023-05-10 16:05:55 +0700

    Update ModalProfile

--- components/ModalProfile.tsx
+++ components/ModalProfile.tsx
@@ -1,10 +1,13 @@
 import { User } from "@/types";
 import { ErrorResponse } from "@/types/error";
+import { appUrl } from "@/utils/constant";
 import errorRes from "@/utils/errorRes";
 import { updateProfile } from "@/utils/fetchApi";
 import {
   Box,
   Button,
+  Flex,
+  Image,
   Input,
   Modal,
   ModalBody,
@@ -30,7 +33,12 @@ interface FileUpload {
   bgImage: File | null;
 }
 
+const baseAvaImage = "https://via.placeholder.com/150";
+const baseBgImage =
+  "https://cdn.pixabay.com/photo/2023/04/30/15/17/saint-tropez-7960722_960_720.jpg";
+
 const ModalProfile = ({ visible, onClose, data, onSuccess }: Props) => {
+  const { name, userEmail, bio, avaImage, bgImage } = data.profile;
   const [files, setFiles] = useState<FileUpload>({
     avaImage: null,
     bgImage: null,
@@ -59,6 +67,7 @@ const ModalProfile = ({ visible, onClose, data, onSuccess }: Props) => {
           title: "Update Failed",
           description: "Upload salah satu file!",
         });
+        setLoading(false);
         return;
       }
 
@@ -95,20 +104,43 @@ const ModalProfile = ({ visible, onClose, data, onSuccess }: Props) => {
         <ModalCloseButton />
         <ModalBody>
           <Box>
+            <Image
+              src={`${appUrl}${bgImage}`}
+              alt="sampul profile"
+              w="full"
+              h="220px"
+              objectFit="cover"
+              fallbackSrc={baseBgImage}
+              borderRadius="lg"
+            />
+            <Flex flexDir="column" alignItems="center" mt={-20} mb={4}>
+              <Image
+                src={`${appUrl}${avaImage}`}
+                alt="sampul profile"
+                boxSize="150px"
+                objectFit="cover"
+                fallbackSrc={baseAvaImage}
+                borderRadius="full"
+              />
+              <Text textAlign="center" fontSize="4xl">
+                {name}
+              </Text>
+              <Text textAlign="center">{userEmail}</Text>
+              <Text textAlign="center" fontSize="sm" fontWeight="light">
+                {bio}
+              </Text>
+            </Flex>
             <Box mb={4}>
-              <pre>
-                <code>{JSON.stringify(data, null, 2)}</code>
-              </pre>
-            </Box>
-            <Box mb={4}>
-              <Text>Upload Foto Profile</Text>
+              <Text>Update Foto Profile</Text>
               <Input name="avaImage" type="file" onChange={handleChangeFile} />
             </Box>
             <Box mb={4}>
-              <Text>Upload Sampul</Text>
+              <Text>Update Sampul</Text>
               <Input name="bgImage" type="file" onChange={handleChangeFile} />
             </Box>
-            <Button onClick={handleSubmit}>Update Profile</Button>
+            <Button onClick={handleSubmit} isLoading={loading}>
+              Update Profile
+            </Button>
           </Box>
         </ModalBody>
       </ModalContent>
--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["192.168.0.102"],
+    domains: ["192.168.0.102", "cdn.pixabay.com"],
   },
   env: {
     app_url: process.env.APP_URL,
